1a861f4c4b2083fe1dd0f4537d4e043dc0b3ad00
Klaus Hartl <klaus.hartl@gmail.com>
2021-07-24 08:10:38 +0200

    Migrate node test to TS

--- test/node.test.ts
+++ test/node.test.ts
@@ -1,22 +1,22 @@
 /* global test, expect */
 /* @jest-environment node */
 
-import Cookies from '../dist/js.cookie.mjs'
+import api from '../src/api'
 
 test('should load the Cookies API', () => {
-  expect(Cookies.get).not.toBeUndefined()
-  expect(Cookies.set).not.toBeUndefined()
-  expect(Cookies.remove).not.toBeUndefined()
+  expect(api.get).not.toBeUndefined()
+  expect(api.set).not.toBeUndefined()
+  expect(api.remove).not.toBeUndefined()
 })
 
 test('should not throw error when attempting to set cookie', () => {
-  expect(() => Cookies.set('foo')).not.toThrow()
+  expect(() => api.set('foo', 'bar')).not.toThrow()
 })
 
 test('should not throw error when attempting to get cookie', () => {
-  expect(() => Cookies.get('foo')).not.toThrow()
+  expect(() => api.get('foo')).not.toThrow()
 })
 
 test('should not throw error when attempting to remove cookie', () => {
-  expect(() => Cookies.remove('foo')).not.toThrow()
+  expect(() => api.remove('foo')).not.toThrow()
 })
