aabbb695224bb56197ec35983e8513446b9d100d
Klaus Hartl <klaus.hartl@gmail.com>
2021-08-22 08:50:27 +0200

    Make converter props optional where necessary

--- types/index.ts
+++ types/index.ts
@@ -12,8 +12,8 @@ export type ReadConverter = (value: string, name?: string) => any
 export type WriteConverter = (value: any, name?: string) => string
 
 export type CookieConverter = object & {
-  read?: ReadConverter
-  write?: WriteConverter
+  read: ReadConverter
+  write: WriteConverter
 }
 
 type CookiesConfig = object & {
@@ -32,7 +32,7 @@ type CookiesApi = object & {
   ) => string | undefined | (object & { [property: string]: any })
   remove: (name: string, attributes?: CookieAttributes) => void
   withAttributes: (attributes: CookieAttributes) => Cookies
-  withConverter: (converter: CookieConverter) => Cookies
+  withConverter: (converter: { write?: WriteConverter, read?: ReadConverter }) => Cookies
 }
 
 export type Cookies = CookiesConfig & CookiesApi
