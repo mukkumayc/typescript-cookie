630851b1bf1479ce77337b735d0c107a2af59c16
Klaus Hartl <klaus.hartl@gmail.com>
2021-08-25 09:04:10 +0200

    Fix references in index entrypoint

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import * as Types from './types/index'
-import Cookies from './src/api'
+import * as api from './src/api'
+import Cookies from './src/compat'
 
 export { Types }
+export { api }
 export default Cookies
