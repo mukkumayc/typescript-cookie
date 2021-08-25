71935bf6e429f2886f084201863dddb3df543aaa
Klaus Hartl <klaus.hartl@gmail.com>
2021-08-25 11:09:45 +0200

    Parameterize E2E tests for testing more browsers

--- test/browser.e2e.js
+++ test/browser.e2e.js
@@ -1,12 +1,8 @@
-/* global beforeAll, afterAll, test, expect */
+/* global describe, beforeAll, afterAll, test, expect */
 const webdriver = require('selenium-webdriver')
 
 // Input capabilities
 const capabilities = {
-  os: 'windows',
-  os_version: '10',
-  browserName: 'chrome',
-  browser_version: 'latest',
   'browserstack.local': 'true',
   build: process.env.BROWSERSTACK_BUILD_NAME,
   project: process.env.BROWSERSTACK_PROJECT_NAME,
@@ -15,21 +11,41 @@ const capabilities = {
   'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY
 }
 
-const driver = new webdriver.Builder()
-  .usingServer('http://hub-cloud.browserstack.com/wd/hub')
-  .withCapabilities(capabilities)
-  .build()
+// Browsers
+const browsers = [
+  {
+    browserName: 'chrome',
+    version: 'latest',
+    os: 'windows',
+    os_version: '10'
+  },
+  {
+    browserName: 'firefox',
+    version: 'latest',
+    os: 'windows',
+    os_version: '10'
+  }
+]
 
-beforeAll(async () => {
-  // HTTP Server should be running on 8099 port of GitHub runner
-  await driver.get('http://localhost:8099/test/browser.e2e.html')
-}, 20000)
+for (const browser of browsers) {
+  describe(`Test E2E in ${browser.browserName}`, () => {
+    const driver = new webdriver.Builder()
+      .usingServer('http://hub-cloud.browserstack.com/wd/hub')
+      .withCapabilities({ ...capabilities, ...browser })
+      .build()
 
-afterAll(async () => {
-  await driver.quit()
-})
+    beforeAll(async () => {
+      // HTTP Server should be running on 8099 port of GitHub runner
+      await driver.get('http://localhost:8099/test/browser.e2e.html')
+    }, 20000)
 
-test('test something', async () => {
-  const cookie = await driver.manage().getCookie('test')
-  expect(cookie.value).toBe('foo')
-}, 5000)
+    afterAll(async () => {
+      await driver.quit()
+    })
+
+    test('setCookie()', async () => {
+      const cookie = await driver.manage().getCookie('test')
+      expect(cookie.value).toBe('foo')
+    }, 5000)
+  })
+}
