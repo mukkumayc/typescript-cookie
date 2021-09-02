37bb365f2ff0ddb088022f84a38c390752cf8ccc
Klaus Hartl <klaus.hartl@gmail.com>
2021-09-02 18:53:40 +0200

    Tidy up codec module

--- src/codec.ts
+++ src/codec.ts
@@ -1,22 +1,22 @@
 import { Decoder, Encoder } from '../types/index'
 
-export const encodeName: (name: string) => string = (name) =>
+export const encodeName: Encoder<string> = (name) =>
   encodeURIComponent(name)
     .replace(/%(2[346B]|5E|60|7C)/g, decodeURIComponent)
     .replace(/[()]/g, escape)
 
 export const encodeValue: Encoder<
 string | number | boolean | undefined | null
-> = (value: any): string => {
+> = (value) => {
   return encodeURIComponent(value as string).replace(
     /%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,
     decodeURIComponent
   )
 }
 
-export const decodeName: (name: string) => string = decodeURIComponent
+export const decodeName: Decoder<string> = decodeURIComponent
 
-export const decodeValue: Decoder<string> = (value: string): string => {
+export const decodeValue: Decoder<string> = (value) => {
   if (value[0] === '"') {
     value = value.slice(1, -1)
   }
