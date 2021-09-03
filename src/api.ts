cc6143aa7e14c77f6d1d1dd4c62f30989f76f50a
Klaus Hartl <klaus.hartl@gmail.com>
2021-09-03 18:30:57 +0200

    Dissolve get/set modules
    Felt quite arbitrary by now..

--- src/api.ts
+++ src/api.ts
@@ -3,16 +3,70 @@ import {
   CookieAttributesConfig,
   CookieCodecConfig,
   CookieDecoding,
-  CookieEncoding
+  CookieEncoding,
+  Decoder
 } from '../types/index'
-import set from './set'
-import get from './get'
 import {
   decodeName as defaultNameDecoder,
   decodeValue as defaultValueDecoder,
   encodeName as defaultNameEncoder,
   encodeValue as defaultValueEncoder
 } from './codec'
+
+function stringifyAttributes (
+  attributes: CookieAttributes & { expires?: any }
+): string {
+  // Copy incoming attributes as to not alter the original object..
+  attributes = Object.assign({}, attributes)
+
+  if (typeof attributes.expires === 'number') {
+    attributes.expires = new Date(Date.now() + attributes.expires * 864e5)
+  }
+  if (attributes.expires != null) {
+    attributes.expires = attributes.expires.toUTCString()
+  }
+
+  return (
+    Object.entries(attributes)
+      .filter(([key, value]: [string, any]) => value != null && value !== false)
+      // Considers RFC 6265 section 5.2:
+      // ...
+      // 3.  If the remaining unparsed-attributes contains a %x3B (";")
+      //     character:
+      // Consume the characters of the unparsed-attributes up to,
+      // not including, the first %x3B (";") character.
+      // ...
+      .map(([key, value]: [string, string | true]) =>
+        value === true ? `; ${key}` : `; ${key}=${value.split(';')[0]}`
+      )
+      .join('')
+  )
+}
+
+type GetReturn<T, R> = [T] extends [undefined]
+  ? object & { [property: string]: string }
+  : R | undefined
+
+function get<T extends string | undefined> (
+  name: T,
+  decodeValue: Decoder<any>,
+  decodeName: Decoder<string>
+): GetReturn<T, typeof decodeValue> {
+  const scan = /(?:^|; )([^=]*)=([^;]*)/g
+  const jar: any = {}
+  let match
+  while ((match = scan.exec(document.cookie)) != null) {
+    try {
+      const found = decodeName(match[1])
+      jar[found] = decodeValue(match[2], found)
+      if (name === found) {
+        break
+      }
+    } catch (e) {}
+  }
+
+  return name != null ? jar[name] : jar
+}
 
 export const DEFAULT_CODEC: CookieCodecConfig<
 string | number | boolean | undefined | null,
@@ -37,7 +91,10 @@ export function setCookie (
     encodeName = defaultNameEncoder
   }: CookieEncoding<any> = {}
 ): string {
-  return set(name, value, attributes, encodeValue, encodeName)
+  return (document.cookie = `${encodeName(name)}=${encodeValue(
+    value,
+    name
+  )}${stringifyAttributes(attributes)}`)
 }
 
 export function getCookie (
