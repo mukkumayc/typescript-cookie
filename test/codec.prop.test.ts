3a78c2abc57e531d6e0693e7f0465cae4242362b
Klaus Hartl <klaus.hartl@gmail.com>
2021-09-13 12:34:25 +0200

    Add further property based tests for encoding

--- test/codec.prop.test.ts
+++ test/codec.prop.test.ts
@@ -1,12 +1,83 @@
 /* global expect, test */
 
-import { assert, property, unicode } from 'fast-check'
-import { decodeValue, encodeValue } from '../src/codec'
+import { ascii, assert, property, unicode } from 'fast-check'
+import { decodeValue, encodeName, encodeValue } from '../src/codec'
+
+const excludeAscii: (value: string) => boolean = (value) =>
+  value.charCodeAt(0) > 127
 
 test('name decode/encode round trip', () => {
   assert(
     property(unicode(), (value) =>
       expect(decodeValue(encodeValue(value))).toBe(value)
-    ), { numRuns: 1000, verbose: true }
+    ),
+    { numRuns: 1000, verbose: true }
+  )
+})
+
+test('value encoding', () => {
+  // ascii minus forbidden characters - no need to encode
+  assert(
+    property(
+      ascii().filter(
+        (value) =>
+          !/\p{Control}/u.test(value) &&
+          ![' ', '"', ',', ';', '\\', '%'].includes(value)
+      ),
+      (value) => expect(encodeValue(value).length).toBe(1)
+    ),
+    { numRuns: 1000, verbose: true }
+  )
+
+  // unicode - will need to be encode
+  assert(
+    property(unicode().filter(excludeAscii), (value) =>
+      expect(encodeValue(value).length).toBeGreaterThan(1)
+    ),
+    { numRuns: 1000, verbose: true }
+  )
+})
+
+test('name encoding', () => {
+  // ascii minus forbidden characters - no need to encode
+  assert(
+    property(
+      ascii().filter(
+        (value) =>
+          !/\p{Control}/u.test(value) &&
+          ![
+            '(',
+            ')',
+            '<',
+            '>',
+            '@',
+            ',',
+            ';',
+            ':',
+            '\\',
+            '"',
+            '/',
+            '[',
+            ']',
+            '?',
+            '=',
+            '{',
+            '}',
+            ' ',
+            '\t',
+            '%'
+          ].includes(value)
+      ),
+      (value) => expect(encodeName(value).length).toBe(1)
+    ),
+    { numRuns: 1000, verbose: true }
+  )
+
+  // unicode - will need to be encode
+  assert(
+    property(unicode().filter(excludeAscii), (value) =>
+      expect(encodeName(value).length).toBeGreaterThan(1)
+    ),
+    { numRuns: 1000, verbose: true }
   )
 })
