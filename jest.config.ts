162a86d3cefdd26ccadd6b807a11f67efc0b6bd5
Klaus Hartl <klaus.hartl@gmail.com>
2022-07-09 08:26:50 +0200

    Make Jest config file TypeScript

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,16 @@
-/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
   clearMocks: true,
   coverageProvider: 'v8',
   moduleFileExtensions: ['js', 'ts'],
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testEnvironmentOptions: {
-    url: 'https://example.org/test' // So we can test setting cookies at the /test path..
+    url: 'https://example.org/test', // So we can test setting cookies at the /test path..
   },
   testMatch: ['**/?(*.)+(spec|test).(mj|[tj])s?(x)'],
-  transform: {}
+  transform: {},
 }
+
+export default config
