d500c818c3335cc4f05b8182ea6a09e70525f7de
Klaus Hartl <klaus.hartl@gmail.com>
2023-02-21 14:43:23 +0100

    Fix import of json file in Rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import typescript from '@rollup/plugin-typescript'
 import terser from '@rollup/plugin-terser'
 import filesize from 'rollup-plugin-filesize'
 import license from 'rollup-plugin-license'
-import pkg from './package.json'
+import pkg from './package.json' assert { type: 'json' }
 
 const licenseBanner = license({
   banner: {
